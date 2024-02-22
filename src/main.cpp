24ec9a4c99e1a17d53961782f41f6c0d07cd0993
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2024-02-22 15:46:49 +0700

    Step 2. Shift Led using bare metal

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
-void setup() {
+// main function
+int main() {
   // init DDRB as output
   DDRB = 0xFF;
   // init port B to 0 
   PORTB = 0x01;
-}
-
-void loop() {
-  // wait 1 second
-  delay(1000);
-  // shift left
-  PORTB = PORTB << 1;
-  // if PORTB is 0, reset to 1
-  if (PORTB == (1 << 5)) {
-    PORTB = 0x01;
+  // loop
+  while (1) {
+    // wait 1 second
+    _delay_ms(1000);
+    // shift left
+    PORTB = PORTB << 1;
+    // if PB5 is 1, reset to 1
+    if (PORTB == (1 << 6)) {
+      PORTB = 0x01;
+    }
   }
+  return 0;
 }
-
